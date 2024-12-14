db044132d28a915be25a9583b66bdc0f0b16d457
Jlz0 <111007912+Jlz0@users.noreply.github.com>
2024-12-14 17:53:38 +0800

    Create evn.js

---
+++ evn.js
@@ -0,0 +1,19 @@
+// API地址
+window.routerBase = 'https://api.v2b.top/'
+window.settings = {
+  // 站点名称
+  title: '派蒙 - Paimom.top',
+  // 站点描述
+  description: '探索无墙世界的钥匙',
+  assets_path: '/assets',
+  // 主题色
+  theme: {
+    color: 'default', //可选default、blue、black、、darkblue
+  },
+  // 版本号
+  version: '0.1.1-dev',
+  // 登陆背景
+  background_url: 'https://acg.suyanw.cn/Anime/',
+  // 站点LOGO
+  logo: 'http://www.paimom.top/favicon.svg',
+}
